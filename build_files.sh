f1ac91b2ec7ad51a3d1fbe26794f57926dfb599b
Gabriel Villafuerte <424103965@pcpuma.acatlan.unam.mx>
2025-12-04 16:05:37 -0600

    Fix archivos estáticos y CSS en Vercel

---
+++ build_files.sh
@@ -0,0 +1,7 @@
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Collecting static files..."
+python3.9 manage.py collectstatic --noinput --clear
+
+echo "Build End"
